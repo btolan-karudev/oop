cb844f3d3ffbd8080003837a237e11761c855a27
Beniamin Tolan <b.tolan@karudev-informatique.fr>
2019-02-13 11:16:23 +0100

    composer autoload

--- bootstrap.php
+++ bootstrap.php
@@ -1,13 +1,14 @@
 <?php
 
+require __DIR__.'/vendor/autoload.php';
 
-spl_autoload_register(function ($className) {
-
-    $path = __DIR__ . '/lib/' . str_replace('\\', '/', $className) . '.php';
-    if (file_exists($path)) {
-        require $path;
-    }
-});
+//spl_autoload_register(function ($className) {
+//
+//    $path = __DIR__ . '/lib/' . str_replace('\\', '/', $className) . '.php';
+//    if (file_exists($path)) {
+//        require $path;
+//    }
+//});
 
 $configuration = [
     'db_dsn' => 'mysql:host=localhost;dbname=oo_battle',
